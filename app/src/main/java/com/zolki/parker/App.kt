3ca2f4a053d92db545d4fb74610c0e4365bcc413
Vitali Yashchuk <dev.yaschchuk@gmail.com>
2021-01-24 18:11:28 +0300

    add add some libraries

---
+++ app/src/main/java/com/zolki/parker/App.kt
@@ -0,0 +1,17 @@
+package com.zolki.parker
+
+import android.app.Application
+import com.zolki.parker.data.di.appModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(appModules)
+        }
+    }
+}
---
+++ app/src/main/java/com/zolki/parker/data/di/AppModule.kt
@@ -0,0 +1,8 @@
+package com.zolki.parker.data.di
+
+import org.koin.dsl.module
+
+@JvmField
+val appModules = module {
+
+}
