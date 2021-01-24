9bd71b2de708f1665365abb371f074c17e1a28e7
Vitali Yashchuk <dev.yaschchuk@gmail.com>
2021-01-24 15:52:28 +0300

    add main activity and update gitignore

---
+++ app/src/main/java/com/zolki/parker/feature/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zolki.parker.feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zolki.parker.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
