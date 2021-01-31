5aef02aaedee99a9110024ca971388d63f63726b
Vitali Yashchuk <dev.yaschchuk@gmail.com>
2021-01-31 11:46:58 +0300

    add map fragment and load map

--- app/src/main/java/com/zolki/parker/feature/map/MapFragment.kt
+++ app/src/main/java/com/zolki/parker/feature/map/MapFragment.kt
@@ -1,6 +1,65 @@
 package com.zolki.parker.feature.map
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.zolki.parker.R
+import com.zolki.parker.databinding.FragmentMapBinding
 
-class MapFragment: Fragment() {
+class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
+
+    private val binding by viewBinding(FragmentMapBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.map.onCreate(savedInstanceState)
+        binding.map.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.map.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.map.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.map.onStop()
+    }
+
+    override fun onPause() {
+        binding.map.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        binding.map.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.map.onLowMemory()
+    }
 }
